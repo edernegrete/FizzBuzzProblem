29260080ff948835930a0313ed49e3adff4bfb57
Eder Negrete <edergonzalezcontacto@gmail.com>
2018-12-24 00:56:19 -0600

    Removed let

--- test/test.js
+++ test/test.js
@@ -6,7 +6,7 @@ const results = require('./results');
 describe('FizzBuzz functions', () => {
   describe('generateList()', () => {
     it('should return the an arr of 100 elements changing the value if the number is multiple of 5 and 3, 3 or 5', () => {
-      let res = index.generateList();
+      const res = index.generateList();
       res.should.be.deep.equal(results.multiarr);
     });
   });
@@ -15,7 +15,7 @@ describe('FizzBuzz functions', () => {
 describe('checker function', () => {
   describe('checker()', () => {
     it('should return "Fizz" if number is a 3 multiple', () => {
-      let res = index.checker(12);
+      const res = index.checker(12);
       res.should.be.equal('Fizz');
     });
   });
@@ -24,7 +24,7 @@ describe('checker function', () => {
 describe('checker function', () => {
   describe('checker()', () => {
     it('should return "Buzz" if number is a 5 multiple', () => {
-      let res = index.checker(20);
+      const res = index.checker(20);
       res.should.be.equal('Buzz');
     });
   });
@@ -33,7 +33,7 @@ describe('checker function', () => {
 describe('checker function', () => {
   describe('checker()', () => {
     it('should return "FizzBuzz" if number is a 3 and 5 multiple', () => {
-      let res = index.checker(30);
+      const res = index.checker(30);
       res.should.be.equal('FizzBuzz');
     });
   });
@@ -42,7 +42,7 @@ describe('checker function', () => {
 describe('checker function', () => {
   describe('checker()', () => {
     it('should return number if number is a not a 3, 5 or 3 and 5 multiple', () => {
-      let res = index.checker(76);
+      const res = index.checker(76);
       res.should.be.equal(76);
     });
   });
